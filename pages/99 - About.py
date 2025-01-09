632974436d66e666b30e69faa64b9ff55c484407
j040n3t0-open <instrusionalert@gmail.com>
2025-01-09 19:30:52 +0000

    Update página about

--- pages/99 - About.py
+++ pages/99 - About.py
@@ -16,7 +16,48 @@ with col1:
   st.image(image, caption='Elastic Certified Professional')
 with col2:
   st.header("João Neto")
-  st.write("**Customer Architect at Elastic**")
-  style = f"padding: 3px; border-radius: 3px; font-size: 14px; line-height: 1.35;"
-  st.markdown(f'<div id="1" style="{style}"><li>GenAI, Data Analyst, Elasticsearch and Observability Certified Engineer</li><li>Elastic Community Organizer (Goiânia)</li><li>2x Gold Community Contributor</li></div>', unsafe_allow_html=True)
+
+
+  style = f"padding: 3px; border-radius: 3px; font-size: 15px; line-height: 1.35;"
+
+  st.markdown("**JOB 👔**")
+  st.markdown(f'''<div id="1" style="{style}">
+              <li>Customer Architect at Elastic</li>
+              <li>Elastic Community Organizer in Goiania</li>
+              <br />
+              </div>''', unsafe_allow_html=True)
+
+  st.markdown("**PREMIOS 🏆**")
+  st.markdown(f'''<div id="1" style="{style}">
+              <li>Elastic Certified Professional of the Year 2023</li>
+              <li>Elastic Gold Contributor 2022</li>
+              <li>Elastic Gold Contributor 2021</li>
+              <li>Elastic Silver Contributor 2020</li>
+              <br />
+              </div>''', unsafe_allow_html=True)
+
+  st.markdown("**CERTIFICAÇÕES 🎯**")
+  st.markdown(f'''<div id="1" style="{style}">
+              <li>Elastic GenAI Associate Certification</li>
+              <li>Elastic Certified Observability Engineer</li>
+              <li>Elastic Certified Analyst</li>
+              <li>Elastic Certified Engineer</li>
+              <br />
+              </div>''', unsafe_allow_html=True)
+
+  st.markdown("**PALESTREI EM... 👨🏻‍🏫**")
+  st.markdown(f'''<div id="1" style="{style}">
+              <li>ElasticON</li>
+              <li>Python Cerrado</li>
+              <li>DevOpsDays BSB</li>
+              <li>DevOpsDays Goiânia</li>
+              <li>TDC</li>
+              <li>FGSL</li>
+              <li>FLISOL</li>
+              <li>Campus Party</li>
+              <li>JoinCommunity</li>
+              <br />
+              </div>''', unsafe_allow_html=True)
+  
+  st.markdown("**CONTATO 👨‍💻**")
   st.write("📨 joao.neto@elastic.co ")
